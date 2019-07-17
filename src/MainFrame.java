9d869195c4a1d0639abe552382e61d97425a097a
yunji <zion830@naver.com>
2019-07-17 21:16:00 +0900

    main 함수가 있는 클래스 분리

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,22 +8,22 @@ import java.io.FileWriter;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
-public class MainPane extends JFrame implements ActionListener {
+public class MainFrame extends JFrame implements ActionListener {
     private JButton addFileBtn = new JButton("파일 선택");
-    private JLabel fileNameLabel = new JLabel(".txt 확장자의 파일을 선택해주세요.");
+    private JLabel fileNameLabel = new JLabel("파일을 선택해주세요.");
     private JButton startExtractBtn = new JButton("초성 추출하기");
     private JFileChooser chooser = new JFileChooser();
 
     private File selectedFile = null;
 
-    private MainPane() {
+    MainFrame() {
         super("초성추출기");
         setSize(500, 100);
         setLocationRelativeTo(null);
     }
 
-    private void createPanel() {
-        MainPane mainPane = new MainPane();
+    void createPanel() {
+        MainFrame mainPane = new MainFrame();
         JPanel jPanel = new JPanel();
 
         jPanel.add(addFileBtn);
@@ -38,6 +38,7 @@ public class MainPane extends JFrame implements ActionListener {
         mainPane.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton clickedBtn = (JButton) e.getSource();
@@ -49,7 +50,7 @@ public class MainPane extends JFrame implements ActionListener {
 
             int result = chooser.showOpenDialog(null);
             String filePath = chooser.getSelectedFile().getPath();
-            fileNameLabel.setText(filePath);
+            fileNameLabel.setText("선택된 파일 : " + filePath);
 
             if (result == JFileChooser.APPROVE_OPTION) {
                 selectedFile = chooser.getSelectedFile();
@@ -63,13 +64,18 @@ public class MainPane extends JFrame implements ActionListener {
 
                 if (result == JFileChooser.APPROVE_OPTION) {
                     String content = FileUtil.getFileContents(selectedFile);
+                    selectedFile = chooser.getSelectedFile();
+                    String path = selectedFile.getPath();
 
                     try {
-                        FileWriter fw = new FileWriter("초성추출_" + selectedFile.getPath() + ".txt");
+                        FileWriter fw = new FileWriter(FileUtil.addFileExtIfNecessary(path, ".txt"));
                         fw.write(content);
                         fw.flush();
                         fw.close();
+
+                        showMessageDialog(null, "파일이 " + path + "에 저장되었습니다.");
                     } catch (Exception e2) {
+                        e2.printStackTrace();
                         showMessageDialog(null, "추출 중 오류가 발생했습니다. 다시 시도해주세요.");
                     }
                 }
@@ -77,8 +83,4 @@ public class MainPane extends JFrame implements ActionListener {
         }
     }
 
-    public static void main(String[] args) {
-        MainPane mainPane = new MainPane();
-        mainPane.createPanel();
-    }
 }
